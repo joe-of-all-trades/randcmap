a23594bda933b434ae580c76189a4ddd99491373
Joe Yeh <joeyeh@gmail.com>
2016-08-18 03:08:20 -0700

    initial commit

---
+++ randcmap.m
@@ -0,0 +1,27 @@
+function outmap = randcmap( levels, maptype )
+%RANDMAP generates colormap with random order
+%   outmat = randmap(levels, matype) generates colormap with randomized
+%   order. For example, rjet = randmap(256, 'jet') generates a randomized
+%   jet colormap with 256 levels. 
+% Copyright, Chao-Yuan Yeh, 2016
+
+if ~any(strcmpi(maptype, {'parula', 'jet', 'hsv', 'hot', 'cool', 'spring', ...
+        'summer', 'autumn', 'winter', 'gray', 'bone', 'copper', 'pink', ...
+        'lines', 'colorcube', 'prism', 'flag', 'white'}))
+    error('Not a supported colormap type')
+end
+
+% Create function handle from a string
+fh = str2func(maptype);
+
+idx = randperm(levels);
+inmap = fh(levels);
+outmap = inmap(idx, :);
+
+% Slow, non-vectorized way
+%for ii = 1:length(idx)
+%    outmap(ii, :) = inmap(idx, :);
+%end
+
+end
+
