dc61e3f4ff1e9f414c36afa69f8e22fddde7b9f9
Joe Yeh <joeyeh@gmail.com>
2016-08-18 03:12:39 -0700

    help edited

--- randcmap.m
+++ randcmap.m
@@ -1,5 +1,5 @@
 function outmap = randcmap( levels, maptype )
-%RANDMAP generates colormap with random order
+%RANDCMAP generates colormap with random order
 %   outmat = randmap(levels, matype) generates colormap with randomized
 %   order. For example, rjet = randmap(256, 'jet') generates a randomized
 %   jet colormap with 256 levels. 
